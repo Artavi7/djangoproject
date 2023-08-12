33f3666825d1276da0bae38354a307437507066f
Artavi7 <tapia.ariel1992@gmail.com>
2023-08-12 11:04:09 -0400

    llegue hasta antes de render

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views  ### esto es lo mismo que--> from myapp import views  
 
 urlpatterns = [
-    path('', views.hello),
-    path('about/', views.about)
+    path('', views.index),
+    path('about/', views.about),
+    path('hello/<str:username>', views.hello),
+    path('projects/', views.projects),
+    path('tasks/<int:id>', views.tasks),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,8 +1,23 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from .models import Project, Task
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
-def hello(request):
-    return HttpResponse("<h2>Dios primero</h2>")
+def index(request):
+    return HttpResponse("Index page")
+
+def hello(request, username):
+    
+    return HttpResponse("<h2>Dios primero</h2><br> <h3>Hola %s </h3>" % username )
 
 def about(request):
     return HttpResponse("About")
+
+def projects(resquest):
+    projects = list(Project.objects.values())
+    return JsonResponse(projects, safe=False)
+
+def tasks(resquest, id):
+    #task = Task.objects.get(id=id) es lo mismo q lo de abajo
+    task = get_object_or_404(Task, id=id)
+    return HttpResponse('task: %s' % task.title)
