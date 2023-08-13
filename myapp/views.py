cac6988b7cdece98634afd8d5865998145babe29
Artavi7 <tapia.ariel1992@gmail.com>
2023-08-13 19:54:34 -0400

    llegue hasta antes de Template inheritance

--- myapp/views.py
+++ myapp/views.py
@@ -1,23 +1,36 @@
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse
 from .models import Project, Task
-from django.shortcuts import get_object_or_404
+from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Index page")
+    title = 'Welcome to Django Course!'
+    return render(request, 'index.html', {
+        'title' : title
+    })
+
+def about(request):
+    username = 'Artavi'
+    return render(request, 'about.html', {
+        'username' : username
+    })
 
 def hello(request, username):
     
     return HttpResponse("<h2>Dios primero</h2><br> <h3>Hola %s </h3>" % username )
 
-def about(request):
-    return HttpResponse("About")
+def projects(request):
+    #projects = list(Project.objects.values())
+    projects = Project.objects.all()
+    return render(request, 'projects.html',{
+        'projects' : projects
+    })
 
-def projects(resquest):
-    projects = list(Project.objects.values())
-    return JsonResponse(projects, safe=False)
-
-def tasks(resquest, id):
+def tasks(request):
     #task = Task.objects.get(id=id) es lo mismo q lo de abajo
-    task = get_object_or_404(Task, id=id)
-    return HttpResponse('task: %s' % task.title)
+    #task = get_object_or_404(Task, id=id)
+    #task = Task.objects.get(id=id)
+    tasks = Task.objects.all()
+    return render(request, 'tasks.html',{
+        'tasks': tasks
+    })
